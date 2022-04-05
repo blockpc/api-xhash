7115adbe0a86cbe8e79be2ace159e2c456f572fe
Juan Carlos Marchant <juan.marchant@gmail.com>
2022-04-04 22:20:10 -0400

    fix zip_codes by zip_code

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Api\XhashController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('zip-codes/{zip_code}', XhashController::class)->name('api.xhash');
+Route::get('zip-code/{zip_code}', XhashController::class)->name('api.xhash');
